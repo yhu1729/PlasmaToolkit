da3e707a530c97a313741fec0e4744869bf736df
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-08-30 18:27:34 -0400

    style: format

--- test/foundation/unit/get_error_message.c
+++ test/foundation/unit/get_error_message.c
@@ -1,14 +1,16 @@
 #include "pt/foundation/error.h"
-#include <string.h>
 #include <assert.h>
+#include <string.h>
 
-int main(void) {
-#define _PT_EXPAND_TO_TEST(tag, message) do { \
-  const char* result = pt_get_error_message(tag); \
-  assert(!strcmp(result, message)); \
-}while(0);
+int
+main(void) {
+#define _PT_EXPAND_TO_TEST(tag, message) \
+  do { \
+    const char* result = pt_get_error_message(tag); \
+    assert(!strcmp(result, message)); \
+  } while (0);
 
-_PT_ERROR_LIST(_PT_EXPAND_TO_TEST)
+  _PT_ERROR_LIST(_PT_EXPAND_TO_TEST)
 
 #undef _PT_EXPAND_TO_TEST
 
