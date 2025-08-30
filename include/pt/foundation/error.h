2abe8259ca0b40beac8acdf3a89ed85d2f1449ef
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-08-30 18:31:59 -0400

    feat: add canary options

--- include/pt/foundation/error.h
+++ include/pt/foundation/error.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "pt/config.h"
 #include "pt/foundation/io.h"
 #include "pt/foundation/tag.h"
 
@@ -8,9 +9,13 @@ const char* pt_get_error_message(const pt_error target);
 pt_error pt_invoke_impl(
   const pt_error result, const char* file, const int line, const char* expr);
 
+#ifdef PT_ABORT_ON_ERROR
 #define pt_invoke(expr) \
   do { \
     if (pt_invoke_impl((expr), __FILE__, __LINE__, #expr)) { \
       abort(); \
     } \
   } while (0)
+#else
+#define pt_invoke(expr) (expr)
+#endif
