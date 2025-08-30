8f5409e766e0eca274f5fd546b97701c3c3f4614
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-08-30 18:33:10 -0400

    refactor: rename canary options

--- include/pt/foundation/error.h
+++ include/pt/foundation/error.h
@@ -9,7 +9,7 @@ const char* pt_get_error_message(const pt_error target);
 pt_error pt_invoke_impl(
   const pt_error result, const char* file, const int line, const char* expr);
 
-#ifdef PT_ABORT_ON_ERROR
+#ifdef PT_OPTION_ABORT_ON_ERROR
 #define pt_invoke(expr) \
   do { \
     if (pt_invoke_impl((expr), __FILE__, __LINE__, #expr)) { \
