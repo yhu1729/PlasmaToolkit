dd6e24b68de866bbb7ffafdf106dfb721c390e06
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 17:48:10 -0400

    feat: add "acquire_buffer"

--- src/container/acquire_buffer.c
+++ src/container/acquire_buffer.c
@@ -2,6 +2,16 @@
 
 pt_error
 pt_acquire_buffer(
-  pt_buffer buffer[1], const size_t size, const size_t alignment) {
+  pt_buffer target[1], const size_t size, const size_t alignment) {
+  *target = malloc(sizeof **target);
+  pt_buffer buffer = *target;
+  buffer->size = size;
+  buffer->alignment = alignment;
+  if (alignment) {
+    pt_aligned_alloc(&(buffer->content), alignment, size);
+  } else {
+    pt_malloc(&(buffer->content), size);
+  }
+
   return PT_TAG_SUCCESS;
 }
