d9a5823e00c2844a2d768de16e945fc7884ad3bc
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 17:24:20 -0400

    style: format

--- src/foundation/malloc_impl.c
+++ src/foundation/malloc_impl.c
@@ -6,7 +6,6 @@ pt_error
 pt_malloc_impl(
   void** target, const size_t size, const char* file, const int line,
   const char* function) {
-
   *target = malloc(size);
   pt_log_debug(
     "malloc: %s:%d  %s  %p+%zu\n", file, line, function, *target, size);
