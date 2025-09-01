e8ccd319b6e36e144936cbce900b7c3a91b0f047
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 17:15:44 -0400

    feat: add "malloc"

---
+++ src/foundation/malloc_impl.c
@@ -0,0 +1,14 @@
+#include "pt/foundation/io.h"
+#include "pt/foundation/memory.h"
+#include <stdlib.h>
+
+pt_error
+pt_malloc_impl(
+  void** target, const size_t size, const char* file, const int line) {
+
+  *target = malloc(size);
+  pt_log_debug(
+    "malloc: %s:%d  %s  %p+%zu\n", file, line, function, *target, size);
+
+  return PT_TAG_SUCCESS;
+}
