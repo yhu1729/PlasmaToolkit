bdca2e9b0018466a00beb821e5e8c94e22da68ca
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 17:34:38 -0400

    refactor: rename "target"

--- include/pt/container/buffer.h
+++ include/pt/container/buffer.h
@@ -12,10 +12,10 @@ struct _pt_buffer_t {
 typedef struct _pt_buffer_t* pt_buffer;
 
 pt_error pt_acquire_buffer(
-  pt_buffer buffer[1], const size_t size, const size_t alignment);
-pt_error pt_release_buffer(pt_buffer buffer);
-pt_error pt_resize_buffer(pt_buffer buffer, const size_t size);
+  pt_buffer target[1], const size_t size, const size_t alignment);
+pt_error pt_release_buffer(pt_buffer target);
+pt_error pt_resize_buffer(pt_buffer target, const size_t size);
 pt_error pt_copy_buffer(pt_buffer dst, const pt_buffer src);
 pt_error pt_move_buffer(pt_buffer dst, pt_buffer src);
 pt_error
-pt_fetch_buffer_element(pt_buffer buffer, const size_t offset, void** element);
+pt_fetch_buffer_element(pt_buffer target, const size_t offset, void** element);
