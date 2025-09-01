736b480f49ead24cd227d657a01cf9776eb65696
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 17:48:56 -0400

    feat: add "release_buffer"

--- src/container/release_buffer.c
+++ src/container/release_buffer.c
@@ -1,6 +1,9 @@
 #include "pt/container/buffer.h"
 
 pt_error
-pt_release_buffer(pt_buffer buffer) {
+pt_release_buffer(pt_buffer target) {
+  free(target->content);
+  free(target);
+
   return PT_TAG_SUCCESS;
 }
