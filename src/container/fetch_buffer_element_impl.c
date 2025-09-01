2e80813789f30d91259fa9d55318f7d99743e17b
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 18:37:21 -0400

    refactor: rename "fetch_buffer_element"

--- src/container/fetch_buffer_element_impl.c
+++ src/container/fetch_buffer_element_impl.c
@@ -1,7 +1,7 @@
 #include "pt/container/buffer.h"
 
 pt_error
-pt_fetch_buffer_element(
+pt_fetch_buffer_element_impl(
   pt_buffer target, const size_t offset, void** element) {
   *((char**)element) = (char*)(target->content) + offset;
 
