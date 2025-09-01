f70e99aab4340ab09217b9fc4783efc399e7031b
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 16:16:12 -0400

    feat: add "sign"

--- include/pt/foundation/math.h
+++ include/pt/foundation/math.h
@@ -4,3 +4,16 @@
 #define pt_max(a, b) (((a) < (b)) ? (b) : (a))
 #define pt_min3(a, b, c) (pt_min(pt_min(a, b), c))
 #define pt_max3(a, b, c) (pt_max(pt_max(a, b), c))
+
+inline static float
+_pt_sign_float(const float x) {
+  return (x >= 0.0f) ? 1.0f : -1.0f;
+}
+
+inline static double
+_pt_sign_double(const double x) {
+  return (x >= 0.0) ? 1.0 : -1.0;
+}
+
+#define pt_sign(x) \
+  _Generic((x), float: _pt_sign_float, double: _pt_sign_double)((x))
