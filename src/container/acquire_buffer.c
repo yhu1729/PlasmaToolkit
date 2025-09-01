e3e3e708ea64fb8a6eef844724ee578aa9851ebc
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 17:23:35 -0400

    feat: add lib "container"

---
+++ src/container/acquire_buffer.c
@@ -0,0 +1,7 @@
+#include "pt/container/buffer.h"
+
+pt_error
+pt_acquire_buffer(
+  pt_buffer buffer[1], const size_t size, const size_t alignment) {
+  return PT_TAG_SUCCESS;
+}
---
+++ src/container/release_buffer.c
@@ -0,0 +1,6 @@
+#include "pt/container/buffer.h"
+
+pt_error
+pt_release_buffer(pt_buffer buffer) {
+  return PT_TAG_SUCCESS;
+}
