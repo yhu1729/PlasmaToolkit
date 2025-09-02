5743a2338b0cb18ed43da904d81962ae88407b2d
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-02 00:58:11 -0400

    refactor: use "size_t"

--- include/pt/algorithm/blas.h
+++ include/pt/algorithm/blas.h
@@ -2,10 +2,11 @@
 
 #include "pt/algorithm/blas.f90.h"
 #include "pt/foundation/tag.h"
+#include <stdlib.h>
 
 extern void dscal(int n, double a, double* x, int inc_x);
 
-pt_error pt_dscal(int n, double a, double* x, int inc_x);
+pt_error pt_dscal(size_t n, double a, double* x, int inc_x);
 
 #define pt_scal(_n, _a, _x, _inc_x) \
   _Generic((_a), double: pt_dscal)((_n), (_a), (_x), (_inc_x))
--- src/algorithm/dscal.c
+++ src/algorithm/dscal.c
@@ -1,8 +1,19 @@
 #include "pt/algorithm/blas.h"
+#include <limits.h>
 
 pt_error
-pt_dscal(int n, double a, double* x, int inc_x) {
-  dscal(n, a, x, inc_x);
+pt_dscal(size_t n, double a, double* x, int inc_x) {
+  if (n < (size_t)INT_MAX) {
+    dscal(n, a, x, inc_x);
+  } else {
+    size_t n_block = n / INT_MAX;
+    size_t n_extra = n % INT_MAX;
+    for (size_t block = 0; block <= n_block; ++block) {
+      int tmp_n = (block == n_block) ? n_extra : INT_MAX;
+      double* tmp_x = x + (block * (size_t)INT_MAX * inc_x);
+      dscal(tmp_n, a, tmp_x, inc_x);
+    }
+  }
 
   return PT_TAG_SUCCESS;
 }
