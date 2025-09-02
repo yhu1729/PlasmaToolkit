9df148832df91f50f59e528b09d1dde44d37fe35
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-01 22:24:44 -0400

    feat: use "pt_free"

--- src/foundation/release_context.c
+++ src/foundation/release_context.c
@@ -1,11 +1,13 @@
 #include "pt/foundation/context.h"
+#include "pt/foundation/error.h"
+#include "pt/foundation/memory.h"
 
 pt_error
 pt_release_context(pt_context target) {
   if (target->type == PT_TAG_LOCAL) {
     struct _pt_context_local_t* c =
       pt_container_of(target, struct _pt_context_local_t, interface.context);
-    free(c);
+    pt_invoke(pt_free(c));
   } else {
     return PT_TAG_INVALID_PARAMETER;
   }
