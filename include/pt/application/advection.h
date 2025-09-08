2064a15aa5bb712b6a0230a4abeb1d11d7219b63
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-08 12:39:03 -0400

    fix: typo

--- include/pt/application/advection.h
+++ include/pt/application/advection.h
@@ -3,9 +3,9 @@
 #include "pt/application/pde.h"
 
 struct _pt_advection_t {
-  struct _pt_pde_t equation;
+  pt_pde equation;
 };
-typedef struct _pt_advection_t& pt_advection;
+typedef struct _pt_advection_t* pt_advection;
 
 pt_error pt_acquire_advection(
   pt_advection target[1], const int variable, const int dimension);
