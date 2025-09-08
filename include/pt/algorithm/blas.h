53ea64c0d489d30c7e3de9ee9ca5e202a9c1c5c5
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-08 13:25:33 -0400

    fix: use "size_t"

--- include/pt/algorithm/blas.h
+++ include/pt/algorithm/blas.h
@@ -6,7 +6,7 @@
 
 extern void dscal(int* n, double* a, double* x, int* inc_x);
 
-pt_error pt_dscal(size_t n, double a, double* x, int inc_x);
+pt_error pt_dscal(size_t n, double a, double* x, size_t inc_x);
 
 #define pt_scal(_n, _a, _x, _inc_x) \
   _Generic((_a), double: pt_dscal)((_n), (_a), (_x), (_inc_x))
