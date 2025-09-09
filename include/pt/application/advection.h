fc8eac4bc3a34562a52a2ba7beeaa84c32929364
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-09 13:28:04 -0400

    feat: add "context"

--- include/pt/application/advection.h
+++ include/pt/application/advection.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "pt/application/pde.h"
+#include "pt/foundation/context.h"
 
 struct _pt_advection_t {
+  pt_context context;
   pt_pde equation;
 };
 typedef struct _pt_advection_t* pt_advection;
 
 pt_error pt_acquire_advection(
-  pt_advection target[1], const int variable, const int dimension);
+  pt_advection target[1], pt_context context, const int variable,
+  const int dimension);
 pt_error pt_release_advection(pt_advection target);
