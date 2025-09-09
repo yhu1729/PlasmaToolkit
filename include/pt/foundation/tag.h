93f40bdfec355cb80103bf53f5d22ca71170bd5a
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-09 13:13:31 -0400

    refactor: rename "_entry"

--- include/pt/foundation/tag.h
+++ include/pt/foundation/tag.h
@@ -11,32 +11,32 @@ typedef enum _pt_tag_t pt_tag;
 
 // clang-format off
 
-#define _PT_ERROR_LIST(entry) \
-  entry(PT_TAG_SUCCESS, "Success") \
-  entry(PT_TAG_INVALID_PARAMETER, "Invalid parameter") \
-  entry(PT_TAG_INVALID_OPERATION, "Invalid operation") \
-  entry(PT_TAG_NULL_POINTER, "Null pointer") \
-  entry(PT_TAG_OUT_OF_MEMORY, "Out of memory") \
-  entry(PT_TAG_OUT_OF_RANGE, "Out of range") \
-  entry(PT_TAG_UNREACHABLE, "Unreachable branch") \
-  entry(PT_TAG_INVALID_OUTPUT, "Invalid output") \
-  entry(PT_TAG_SINGULAR_MATRIX, "Singular matrix")
+#define _PT_ERROR_LIST(_entry) \
+  _entry(PT_TAG_SUCCESS, "Success") \
+  _entry(PT_TAG_INVALID_PARAMETER, "Invalid parameter") \
+  _entry(PT_TAG_INVALID_OPERATION, "Invalid operation") \
+  _entry(PT_TAG_NULL_POINTER, "Null pointer") \
+  _entry(PT_TAG_OUT_OF_MEMORY, "Out of memory") \
+  _entry(PT_TAG_OUT_OF_RANGE, "Out of range") \
+  _entry(PT_TAG_UNREACHABLE, "Unreachable branch") \
+  _entry(PT_TAG_INVALID_OUTPUT, "Invalid output") \
+  _entry(PT_TAG_SINGULAR_MATRIX, "Singular matrix")
 
-#define _PT_VERBOSITY_LIST(entry) \
-  entry(PT_TAG_VERBOSITY_NONE, 0, "None") \
-  entry(PT_TAG_VERBOSITY_ERROR, 1, "Error") \
-  entry(PT_TAG_VERBOSITY_WARNING, 2, "Warning") \
-  entry(PT_TAG_VERBOSITY_INFO, 3, "Info") \
-  entry(PT_TAG_VERBOSITY_DEBUG, 4, "Debug")
+#define _PT_VERBOSITY_LIST(_entry) \
+  _entry(PT_TAG_VERBOSITY_NONE, 0, "None") \
+  _entry(PT_TAG_VERBOSITY_ERROR, 1, "Error") \
+  _entry(PT_TAG_VERBOSITY_WARNING, 2, "Warning") \
+  _entry(PT_TAG_VERBOSITY_INFO, 3, "Info") \
+  _entry(PT_TAG_VERBOSITY_DEBUG, 4, "Debug")
 
-#define _PT_MEMORY_TYPE_LIST(entry) \
-  entry(PT_TAG_CPU, "CPU") \
-  entry(PT_TAG_GPU_NVIDIA, "GPU/NVIDIA") \
-  entry(PT_TAG_GPU_AMD, "GPU/AMD")
+#define _PT_MEMORY_TYPE_LIST(_entry) \
+  _entry(PT_TAG_CPU, "CPU") \
+  _entry(PT_TAG_GPU_NVIDIA, "GPU/NVIDIA") \
+  _entry(PT_TAG_GPU_AMD, "GPU/AMD")
 
-#define _PT_PDE_TYPE_LIST(entry) \
-  entry(PT_TAG_ADVECTION, "Advection equation") \
-  entry(PT_TAG_EULER, "Euler equation")
+#define _PT_PDE_TYPE_LIST(_entry) \
+  _entry(PT_TAG_ADVECTION, "Advection equation") \
+  _entry(PT_TAG_EULER, "Euler equation")
 
 // clang-format on
 
