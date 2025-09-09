003c54db41b5be04facd466f422a4a84f7988145
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-09 13:28:13 -0400

    style: format

--- include/pt/algorithm/linear_solver.h
+++ include/pt/algorithm/linear_solver.h
@@ -4,12 +4,12 @@
 #include "pt/foundation/tag.h"
 
 struct _pt_linear_solver {
-  pt_linear_solver_type type;
   pt_context context;
+  pt_linear_solver_type type;
 };
 typedef struct _pt_linear_solver* pt_linear_solver;
 
 pt_error pt_acquire_linear_solver(
-  pt_linear_solver target[1], const pt_linear_solver_type type,
-  pt_context context);
+  pt_linear_solver target[1], pt_context context,
+  const pt_linear_solver_type type);
 pt_error pr_release_linear_solver(pt_linear_solver target);
