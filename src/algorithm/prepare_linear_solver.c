bfbbc446902077fbf7f4535e557d1e786f30cf70
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-21 19:59:56 -0400

    fix: grow pivot array length

--- src/algorithm/prepare_linear_solver.c
+++ src/algorithm/prepare_linear_solver.c
@@ -10,15 +10,24 @@ pt_prepare_linear_solver(
     return PT_TAG_INVALID_PARAMETER;
   }
 
+  bool flag_grow_pivot = (n_eqn > target->info.n_eqn);
   target->info.n_eqn = n_eqn;
   target->info.n_rhs = n_rhs;
 
   if (target->type == PT_TAG_DIRECT) {
-    pt_invoke(pt_calloc(&(target->info.pivot), n_eqn, sizeof(int)));
+    if (!(target->info.pivot)) {
+      pt_invoke(pt_calloc(&(target->info.pivot), n_eqn, sizeof(int)));
+    } else {
+      if (flag_grow_pivot) {
+        pt_invoke(pt_realloc(&(target->info.pivot), n_eqn * sizeof(int)));
+      }
+    }
 
     if (A) {
       pt_invoke(pt_getrf(n_eqn, n_eqn, A, n_eqn, target->info.pivot));
       target->info.pivot_ready = true;
+    } else {
+      target->info.pivot_ready = false;
     }
   } else {
     return PT_TAG_INVALID_PARAMETER;
