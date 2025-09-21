175935336b536bed0ddfd5fa2ea3e30daf1693e8
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-21 17:19:31 -0400

    feat: add "dgesv"

--- include/pt/algorithm/lapack.h
+++ include/pt/algorithm/lapack.h
@@ -2,12 +2,25 @@
 
 #include "pt/foundation/tag.h"
 
+#define dgesv(...) dgesv_(__VA_ARGS__)
+
 #define dgetrf(...) dgetrf_(__VA_ARGS__)
+
+extern void dgesv_(
+  int* n, int* n_rhs, double* A, int* ld_A, int* pivot, double* B, int* ld_B,
+  int* info);
 
 extern void
 dgetrf_(int* m, int* n, double* A, int* ld_A, int* pivot, int* info);
 
+pt_error pt_dgesv(
+  int n, int n_rhs, double* A, int ld_A, int* pivot, double* B, int ld_B);
+
 pt_error pt_dgetrf(int m, int n, double* A, int ld_A, int* pivot);
+
+#define pt_gesv(_n, _n_rhs, _A, _ld_A, _pivot, _B, _ld_B) \
+  _Generic((*(_A)), double: pt_dgesv)( \
+    (_n), (_n_rhs), (_A), (_ld_A), (_pivot), (_B), (_ld_B))
 
 #define pt_getrf(_m, _n, _A, _ld_A, _pivot) \
   _Generic((*(_A)), double: pt_dgetrf)((_m), (_n), (_A), (_ld_A), (_pivot))
---
+++ src/algorithm/dgesv.c
@@ -0,0 +1,23 @@
+#include "pt/algorithm/lapack.h"
+#include <stdlib.h>
+
+pt_error
+pt_dgesv(
+  int n, int n_rhs, double* A, int ld_A, int* pivot, double* B, int ld_B) {
+  if ((A == NULL) || (pivot == NULL) || (B == NULL)) {
+    return PT_TAG_NULL_POINTER;
+  }
+  if ((n == 0) || (n_rhs == 0) || (ld_A == 0) || (ld_B == 0)) {
+    return PT_TAG_INVALID_PARAMETER;
+  }
+
+  int info;
+  dgesv(&n, &n_rhs, A, &ld_A, pivot, B, &ld_B, &info);
+  if (info < 0) {
+    return PT_TAG_INVALID_OUTPUT;
+  } else if (info > 0) {
+    return PT_TAG_SINGULAR_MATRIX;
+  }
+
+  return PT_TAG_SUCCESS;
+}
