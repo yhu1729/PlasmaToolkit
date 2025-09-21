80e27a5514b9732103e84da1b77299a5e8aef17a
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-21 18:38:41 -0400

    fix: typo

--- include/pt/algorithm/linear_solver.h
+++ include/pt/algorithm/linear_solver.h
@@ -12,4 +12,4 @@ typedef struct _pt_linear_solver* pt_linear_solver;
 pt_error pt_acquire_linear_solver(
   pt_linear_solver target[1], pt_context context,
   const pt_linear_solver_type type);
-pt_error pr_release_linear_solver(pt_linear_solver target);
+pt_error pt_release_linear_solver(pt_linear_solver target);
