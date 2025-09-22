09280dad71a7957f046a1962be1e4dbec0e40879
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-21 20:01:03 -0400

    test: add tests for "linear_solver"

--- test/algorithm/unit/linear_solver.c
+++ test/algorithm/unit/linear_solver.c
@@ -30,6 +30,19 @@ test_direct_4(void) {
   }
   pt_test_invoke(PT_TAG_SUCCESS, pt_apply(target, A, B));
 
+  for (int row = 0; row < 4; ++row) {
+    for (int col = 0; col < 4; ++col) {
+      int value = rand();
+      A[row + col * 4] = ((double)(value) / (double)(RAND_MAX)) * 2.0 - 1.0;
+    }
+    for (int col = 0; col < 2; ++col) {
+      int value = rand();
+      B[row + col * 4] = ((double)(value) / (double)(RAND_MAX)) * 2.0 - 1.0;
+    }
+  }
+  pt_test_invoke(PT_TAG_SUCCESS, pt_prepare(target, 4, 2, A));
+  pt_test_invoke(PT_TAG_SUCCESS, pt_apply(target, A, B));
+
   pt_test_invoke(PT_TAG_SUCCESS, pt_free(A));
   pt_test_invoke(PT_TAG_SUCCESS, pt_free(B));
   pt_test_invoke(PT_TAG_SUCCESS, pt_release(target));
