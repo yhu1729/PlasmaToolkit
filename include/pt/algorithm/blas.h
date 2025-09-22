8704ab356d19c60922b2d23901358654c48b6539
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-21 20:30:46 -0400

    feat: add "dgemm"

--- include/pt/algorithm/blas.h
+++ include/pt/algorithm/blas.h
@@ -3,11 +3,21 @@
 #include "pt/foundation/tag.h"
 #include <stdlib.h>
 
+// level 1: dscal
 #define dscal(...) dscal_(__VA_ARGS__)
-
 extern void dscal_(int* n, double* a, double* x, int* inc_x);
-
 pt_error pt_dscal(int n, double a, double* x, int inc_x);
 
+// level 3: dgemm
+#define dgemm(...) dgemm_(__VA_ARGS__)
+extern void dgemm_(
+  char* transpose_A, char* transpose_B, int* m, int* n, int* k, double* alpha,
+  double* A, int* ld_A, double* B, int* ld_B, double* beta, double* C,
+  int* ld_C);
+pt_error pt_dgemm(
+  char* transpose_A, char* transpose_B, int m, int n, int k, double alpha,
+  double* A, int ld_A, double* B, int ld_B, double beta, double* C, int ld_C);
+
+// level 1: scal
 #define pt_scal(_n, _a, _x, _inc_x) \
   _Generic((_a), double: pt_dscal)((_n), (_a), (_x), (_inc_x))
