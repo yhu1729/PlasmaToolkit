cc3bd3db1abacf9c05473be314912f0d115227c7
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-06 17:06:52 -0400

    feat: add module core/type

---
+++ include/pt/core/type.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "pt/config.h"
+#include <stdint.h>
+
+typedef uint_fast8_t pt_u8;
+typedef uint_fast16_t pt_u16;
+typedef uint_fast32_t pt_u32;
+typedef uint_fast64_t pt_u64;
+typedef int_fast8_t pt_i8;
+typedef int_fast16_t pt_i16;
+typedef int_fast32_t pt_i32;
+typedef int_fast64_t pt_i64;
+typedef float pt_f32;
+typedef double pt_f64;
+typedef long double pt_f128;
+
+typedef pt_i32 pt_index;
