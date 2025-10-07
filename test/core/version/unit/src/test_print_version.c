6f945a80ab68975bc19366894f9009a27f0bb051
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-07 18:52:45 -0400

    test: add tests

--- test/core/version/unit/src/test_print_version.c
+++ test/core/version/unit/src/test_print_version.c
@@ -9,6 +9,15 @@ test_print_full(void) {
   pt_check_ok();
 }
 
-PT_TEST_LIST({test_print_full, "print full version"});
+void
+test_print_short(void) {
+  pt_print_version(false);
+
+  pt_check_ok();
+}
+
+PT_TEST_LIST(
+  {test_print_full, "print full version number"},
+  {test_print_short, "print short version number"});
 
 PT_TEST_UNIT_MAIN
