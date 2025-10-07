a42274fee518d692c5a5450f6d348605576e2d52
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-07 17:57:50 -0400

    fix: remove semicolon

--- include/pt/test/unit.h
+++ include/pt/test/unit.h
@@ -9,6 +9,6 @@ struct _pt_test_list_item_t {
 extern const struct _pt_test_list_item_t pt_test_list[];
 #define PT_TEST_LIST_IMPL const struct _pt_test_list_item_t _pt_test_list[]
 #define PT_TEST_LIST(...) \
-  PT_TEST_LIST_IMPL = {__VA_ARGS__ __VA_OPT__(, ){NULL, NULL}};
+  PT_TEST_LIST_IMPL = {__VA_ARGS__ __VA_OPT__(, ){NULL, NULL}}
 
 #include "pt/test/unit/main.h"
