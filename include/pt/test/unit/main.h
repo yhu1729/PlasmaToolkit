54c8b284f868dd00b5fc8a1d2dfd78df1e615b58
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-08 13:07:22 -0400

    feat: improve output

--- include/pt/test/unit/main.h
+++ include/pt/test/unit/main.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "pt/core/log.h"
 #include <stdlib.h>
 
 #define PT_TEST_UNIT_MAIN \
@@ -13,7 +14,9 @@
     void (*handle)(void); \
     for (int index = 0; index < list_size; ++index) { \
       handle = _pt_test_list[index].handle; \
+      pt_emit_log_info("TEST: %s", _pt_test_list[index].name); \
       handle(); \
+      pt_emit_log_info("PASS: %s", _pt_test_list[index].name); \
     } \
     return EXIT_SUCCESS; \
   }
