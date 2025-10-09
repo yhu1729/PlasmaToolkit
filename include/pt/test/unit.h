b671423fe13846b1ae9573692354861f30aa5b57
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-08 22:51:58 -0400

    style: format

--- include/pt/test/unit.h
+++ include/pt/test/unit.h
@@ -6,8 +6,11 @@ struct pt_test_list_item_t {
   void (*handle)(void);
   const char* name;
 };
+
 extern const struct pt_test_list_item_t pt_test_list[];
+
 #define PT_TEST_LIST_IMPL const struct pt_test_list_item_t pt_test_list[]
+
 #define PT_TEST_LIST(...) \
   PT_TEST_LIST_IMPL = {__VA_ARGS__ __VA_OPT__(, ){NULL, NULL}}
 
