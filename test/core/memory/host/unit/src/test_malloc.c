bc83d04d8b4f62f82803b7414a6e0415460eee51
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-09 23:31:51 -0400

    test: improve tests

--- test/core/memory/host/unit/src/test_malloc.c
+++ test/core/memory/host/unit/src/test_malloc.c
@@ -6,7 +6,8 @@
 void
 test_allocate_small(void) {
   char* target;
-  pt_expect(PT_TAG_SUCCESS, pt_malloc(&target, 16));
+  pt_expect(PT_TAG_SUCCESS, pt_malloc(&target, 16 * sizeof(char)));
+  target[15] = 'a';
   pt_free(target);
 
   pt_check_ok();
@@ -14,8 +15,9 @@ test_allocate_small(void) {
 
 void
 test_allocate_large(void) {
-  char* target;
-  pt_expect(PT_TAG_SUCCESS, pt_malloc(&target, 1024));
+  double* target;
+  pt_expect(PT_TAG_SUCCESS, pt_malloc(&target, 1024 * sizeof(double)));
+  target[1023] = 1.0;
   pt_free(target);
 
   pt_check_ok();
