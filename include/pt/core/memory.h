00cf87498718dc0639ff89acc93df83e75771d7c
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-25 00:18:37 -0400

    fix: remove "defined"

--- include/pt/core/memory.h
+++ include/pt/core/memory.h
@@ -4,7 +4,7 @@
 #include "pt/core/memory/device.h"
 #include "pt/core/memory/host.h"
 
-#if PT_ON_HOST
+#ifndef __CUDA_ARCH__
 #define pt_free(...) pt_free_h(__VA_ARGS__)
 #define pt_malloc(...) pt_malloc_h(__VA_ARGS__)
 #define pt_calloc(...) pt_calloc_h(__VA_ARGS__)
