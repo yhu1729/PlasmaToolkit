cc42fe9a2b3633e0d6367063fe7962d4dfcdee00
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-28 18:19:51 -0400

    feat: support NCCL

--- src/core/context/nccl/acquire_context_nccl.c
+++ src/core/context/nccl/acquire_context_nccl.c
@@ -1,8 +1,16 @@
 #include "pt/core/context/nccl.h"
+#include "pt/core/memory.h"
 
 pt_status
 pt_acquire_context_nccl(pt_context_nccl target[1]) {
   PT_STATUS(status);
+
+  pt_safe_invoke(pt_malloc(target, sizeof **target));
+  pt_context_nccl handle = *target;
+  handle->active = false;
+  handle->rank = 0;
+  handle->size = 0;
+  handle->network = NULL;
 
   return status;
 }
--- src/core/context/nccl/release_context_nccl.c
+++ src/core/context/nccl/release_context_nccl.c
@@ -1,8 +1,14 @@
 #include "pt/core/context/nccl.h"
+#include "pt/core/memory.h"
 
 pt_status
 pt_release_context_nccl(pt_context_nccl target) {
   PT_STATUS(status);
+
+  if(target->active) {
+    pt_safe_invoke(pt_finalize_context_nccl(target));
+  }
+  pt_safe_invoke(pt_free(target));
 
   return status;
 }
