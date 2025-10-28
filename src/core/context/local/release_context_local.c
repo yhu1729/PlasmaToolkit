8f1195161c568c1c31e8ee87ae89438171a83ccd
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-28 18:50:25 -0400

    fix: safe release

--- src/core/context/local/release_context_local.c
+++ src/core/context/local/release_context_local.c
@@ -5,6 +5,9 @@ pt_status
 pt_release_context_local(pt_context_local target) {
   PT_STATUS(status);
 
+  if(target->active){
+    pt_safe_invoke(pt_finalize(target));
+  }
   pt_safe_invoke(pt_free(target));
 
   return status;
--- src/core/context/mpi/release_context_mpi.c
+++ src/core/context/mpi/release_context_mpi.c
@@ -5,6 +5,9 @@ pt_status
 pt_release_context_mpi(pt_context_mpi target) {
   PT_STATUS(status);
 
+  if(target->active){
+    pt_safe_invoke(pt_finalize(target));
+  }
   pt_safe_invoke(pt_free(target));
 
   return status;
