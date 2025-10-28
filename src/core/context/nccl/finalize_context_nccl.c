853bc1db9fffb6091d46f16165168de1f2fd74e6
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-28 19:19:13 -0400

    feat: support NCCL

--- src/core/context/nccl/finalize_context_nccl.c
+++ src/core/context/nccl/finalize_context_nccl.c
@@ -4,5 +4,18 @@ pt_status
 pt_finalize_context_nccl(pt_context_nccl target) {
   PT_STATUS(status);
 
+  if (!(target->active)) {
+    status.code = PT_TAG_INVALID_NETWORK;
+
+    return status;
+  }
+
+  target->active = false;
+  target->rank = 0;
+  target->size = 0;
+
+  ncclCommDestroy(target->network);
+  target->network = NULL;
+
   return status;
 }
