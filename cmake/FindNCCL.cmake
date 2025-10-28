6994ff895732e417e5115eca56c3f163fa1e29a4
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-28 17:24:33 -0400

    build: improve FindNCCL

--- cmake/FindNCCL.cmake
+++ cmake/FindNCCL.cmake
@@ -1,15 +1,12 @@
-set(_NCCL_INCLUDE_DIR $ENV{NCCL_INCLUDE_DIR} CACHE PATH "")
-set(_NCCL_LIB_DIR $ENV{NCCL_LIB_DIR} CACHE PATH "")
-set(_NCCL_VERSION $ENV{NCCL_VERSION} CACHE STRING "")
+find_package(CUDAToolkit REQUIRED)
+
 set(NCCL_LIB_NAME "nccl")
+set(NCCL_ROOT $ENV{NVHPC_ROOT}/comm_libs/nccl)
 
 list(APPEND CMAKE_PREFIX_PATH ${NCCL_ROOT})
-find_path(NCCL_INC_DIR NAMES nccl.h HINTS ${_NCCL_INCLUDE_DIR})
+find_path(NCCL_INC_DIR NAMES nccl.h)
 
-if(_NCCL_VERSION)
-  set(CMAKE_FIND_LIBRARY_SUFFIXES ".so.${_NCCL_VERSION}" ${CMAKE_FIND_LIBRARY_SUFFIXES})
-endif()
-find_library(NCCL_LIB NAMES ${NCCL_LIB_NAME} HINTS ${_NCCL_LIB_DIR})
+find_library(NCCL_LIB NAMES ${NCCL_LIB_NAME})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(NCCL DEFAULT_MSG NCCL_INC_DIR NCCL_LIB)
@@ -22,7 +19,7 @@ if(NCCL_FOUND)
   include(CheckCXXSymbolExists)
   check_cxx_symbol_exists(NCCL_VERSION_CODE nccl.h NCCL_VERSION_DEFINED)
   if(NCCL_VERSION_DEFINED)
-    set(file "${PROJECT_BINARY_DIR}/detect_nccl_version.cc")
+    set(file "${PROJECT_BINARY_DIR}/detect_nccl_version.cu")
     file(WRITE ${file} "
       #include <nccl.h>
       int main()
@@ -39,10 +36,8 @@ if(NCCL_FOUND)
   endif()
 
   set(CMAKE_REQUIRED_INCLUDES ${OLD_CMAKE_REQUIRED_INCLUDES})
-  message(STATUS "Found NCCL (include: ${NCCL_INC_DIR}, library: ${NCCL_LIB})")
-  mark_as_advanced(NCCL_ROOT_DIR NCCL_INC_DIR NCCL_LIB)
 
-  add_library(NCCL INTERFACE IMPORTED)
-  set_property(TARGET NCCL PROPERTY INTERFACE_COMPILE_OPTIONS -I${NCCL_INC_DIR})
-  set_property(TARGET NCCL PROPERTY INTERFACE_LINK_LIBRARIES -L${NCCL_LIB}
+  add_library(NCCL INTERFACE)
+  set_property(TARGET NCCL PROPERTY INTERFACE_COMPILE_OPTIONS -I${NCCL_INC_DIR} -I${CUDAToolkit_INCLUDE_DIRS})
+  set_property(TARGET NCCL PROPERTY INTERFACE_LINK_LIBRARIES -L${NCCL_LIB} -L${CUDAToolkit_LIBRARY_DIR})
 endif()
