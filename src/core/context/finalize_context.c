e3a9eda007ecefc424b45216ea408fbfa02befc2
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-28 19:23:03 -0400

    feat: add checks

--- src/core/context/finalize_context.c
+++ src/core/context/finalize_context.c
@@ -5,6 +5,12 @@ pt_status
 pt_finalize_context(pt_context target) {
   PT_STATUS(status);
 
+  if (!(target->active)) {
+    status.code = PT_TAG_INVALID_NETWORK;
+
+    return status;
+  }
+
   pt_safe_invoke(pt_finalize(target->interface, target->type));
   target->active = false;
 
--- src/core/context/initialize_context.c
+++ src/core/context/initialize_context.c
@@ -5,6 +5,12 @@ pt_status
 pt_initialize_context(pt_context target, pt_context leader) {
   PT_STATUS(status);
 
+  if (target->active) {
+    status.code = PT_TAG_INVALID_NETWORK;
+
+    return status;
+  }
+
   pt_safe_invoke(pt_initialize(target->interface, target->type, leader));
   target->active = true;
 
