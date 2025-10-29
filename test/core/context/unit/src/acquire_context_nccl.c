501ce4e990e64335de2dfab82aef332c94c4f09b
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-28 21:03:40 -0400

    test: add tests

---
+++ test/core/context/unit/src/acquire_context_nccl.c
@@ -0,0 +1,25 @@
+#include "pt/config.h"
+#include "pt/core/context/mpi.h"
+#include "pt/core/context/nccl.h"
+#include "pt/core/macro.h"
+#include "pt/test/check.h"
+#include "pt/test/expect.h"
+#include "pt/test/unit.h"
+
+void
+test_single(void) {
+  pt_context_mpi leader;
+  pt_acquire(&leader);
+  pt_context_nccl target;
+  pt_expect(PT_TAG_SUCCESS, pt_acquire_context_nccl(&target, leader));
+  pt_check(!(target->active));
+  pt_check_same(target->rank, 0);
+  pt_check_same(target->size, 0);
+  pt_release(target);
+  pt_release(leader);
+  pt_check_ok();
+}
+
+PT_TEST_LIST({test_single, "Acquire NCCL context with single device"});
+
+PT_TEST_UNIT_MAIN
