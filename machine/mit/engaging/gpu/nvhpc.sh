f8ed23bfb6d545801aceffde20615053b4b9b2aa
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-10-31 15:27:05 -0400

    chore: fix linker path

--- machine/mit/engaging/gpu/nvhpc.sh
+++ machine/mit/engaging/gpu/nvhpc.sh
@@ -3,6 +3,7 @@ module restore
 module load nvhpc/24.5
 
 export CUDA_HOME=/orcd/software/community/001/rocky8/nvhpc/2024_245/Linux_x86_64/24.5/cuda
+export LD_LIBRARY_PATH=/orcd/software/community/001/rocky8/nvhpc/2024_245/Linux_x86_64/24.5/comm_libs/mpi/lib:$LD_LIBRARY_PATH
 
 # compiler
 export CC=mpicc
