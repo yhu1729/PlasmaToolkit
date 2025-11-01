a72737ddc7301445615373ec5cb65b7385b9ef39
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-11-01 16:44:16 -0400

    test: add tests

---
+++ test/core/memory/device/unit/src/free_d.c
@@ -0,0 +1,28 @@
+#include "pt/core/memory/device.h"
+#include "pt/test/check.h"
+#include "pt/test/expect.h"
+#include "pt/test/unit.h"
+
+void
+test_free_small(void) {
+  char* target;
+  pt_malloc_d(&target, 16);
+  pt_expect(PT_TAG_SUCCESS, pt_free_d(target));
+
+  pt_check_ok();
+}
+
+void
+test_free_large(void) {
+  char* target;
+  pt_malloc_d(&target, 1024);
+  pt_expect(PT_TAG_SUCCESS, pt_free_d(target));
+
+  pt_check_ok();
+}
+
+PT_TEST_LIST(
+  {test_free_small, "Free small chunks"},
+  {test_free_large, "Free large chunks"});
+
+PT_TEST_UNIT_MAIN
