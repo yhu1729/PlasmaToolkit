469c520c3dcc295ef3a97f6771cdb3ce7b50c368
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-11-01 17:39:43 -0400

    test: add tests

---
+++ test/core/memory/device/unit/src/test_calloc_d.c
@@ -0,0 +1,30 @@
+#include "pt/core/memory/device.h"
+#include "pt/test/check.h"
+#include "pt/test/expect.h"
+#include "pt/test/unit.h"
+
+void
+test_small(void) {
+  char* target;
+  pt_expect(PT_TAG_SUCCESS, pt_calloc_d(&target, 16, sizeof(char)));
+  target[15] = 'a';
+  pt_free_d(target);
+
+  pt_check_ok();
+}
+
+void
+test_large(void) {
+  double* target;
+  pt_expect(PT_TAG_SUCCESS, pt_calloc_d(&target, 1024, sizeof(double)));
+  target[1023] = 1.0;
+  pt_free_d(target);
+
+  pt_check_ok();
+}
+
+PT_TEST_LIST(
+  {test_small, "Allocate small chunks"},
+  {test_large, "Allocate large chunks"});
+
+PT_TEST_UNIT_MAIN
