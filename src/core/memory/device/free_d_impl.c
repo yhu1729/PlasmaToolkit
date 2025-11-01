183167ef51a082073bb1855e2f4b9a9119bf4a78
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-11-01 16:31:31 -0400

    feat: add free_d_impl

---
+++ src/core/memory/device/free_d_impl.c
@@ -0,0 +1,15 @@
+#include "pt/core/log.h"
+#include "pt/core/memory/device.h"
+#include <cuda.h>
+
+pt_status
+pt_free_d_impl(
+  void* target, const char* function, const char* file, const int line) {
+  PT_STATUS(status);
+
+  pt_emit_log_debug(
+    "free %p in \"%s\" at %s:%d", target, function, file, line);
+  cudaFree(target);
+
+  return status;
+}
