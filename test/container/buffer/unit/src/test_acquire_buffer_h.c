12e5998b91b2cb55d65557f5a028b7c576c3ada5
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-11-03 11:47:27 -0500

    test: add tests

---
+++ test/container/buffer/unit/src/test_acquire_buffer_h.c
@@ -0,0 +1,29 @@
+#include "pt/container/buffer/host.h"
+#include "pt/core/macro.h"
+#include "pt/test/check.h"
+#include "pt/test/expect.h"
+#include "pt/test/unit.h"
+
+void
+test_small(void) {
+  pt_buffer_h target;
+  pt_expect(PT_TAG_SUCCESS, pt_acquire(&target, 16));
+  pt_release(target);
+
+  pt_check_ok();
+}
+
+void
+test_large(void) {
+  pt_buffer_h target;
+  pt_expect(PT_TAG_SUCCESS, pt_acquire(&target, 131072));
+  pt_release(target);
+
+  pt_check_ok();
+}
+
+PT_TEST_LIST(
+  {test_small, "Acquire a small buffer"},
+  {test_large, "Acquire a large buffer"});
+
+PT_TEST_UNIT_MAIN
