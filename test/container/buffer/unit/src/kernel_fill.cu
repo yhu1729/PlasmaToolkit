3b34833e8657355af41b5f4bd374c9a3ddc8171c
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-11-10 18:39:53 -0500

    fix: test kernel

--- test/container/buffer/unit/src/kernel_fill.cu
+++ test/container/buffer/unit/src/kernel_fill.cu
@@ -1,12 +1,17 @@
 #include "pt/container/buffer/device.h"
 #include <cuda.h>
 
-__global__ void
+__device__ void
 kernel_fill(const size_t n, char* buffer) {
-  cuMemsetD8(buffer, 0, n);
+  for (int index = 0; index < n; ++index) {
+    buffer[index] = 0;
+  }
 }
 
 extern "C" void
 invoke_kernel_fill(pt_buffer_d target) {
-  kernel_fill(target->length, target->content);
+  // method 1: invoke device kernel
+  kernel_fill<<<1, 1>>>(target->length, target->content);
+  // method 2: use CUDA Driver API
+  cuMemsetD8((CUdeviceptr)(target->content), 0, target->length);
 }
